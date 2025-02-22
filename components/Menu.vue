edb5385a88f5aaad16a1a16226fb16714125568d
Camellia <56206613+sialiss@users.noreply.github.com>
2025-02-22 20:42:29 +0300

    update

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,53 +1,74 @@
 <template>
-	<nav class="navbar">
-		<ul>
-			<li>
-				<NuxtLink to="/" exact-active-class="active">Главная</NuxtLink>
-			</li>
-			<!-- <li><NuxtLink to="/about" exact-active-class="active">О нас</NuxtLink></li> -->
-			<li>
-				<NuxtLink to="/cube" exact-active-class="active">Куб</NuxtLink>
-			</li>
-			<li>
-				<NuxtLink to="/cars" exact-active-class="active">Машинки</NuxtLink>
-			</li>
-		</ul>
-	</nav>
+	<div class="minimenu">
+		<div class="account">
+			<NuxtLink to="/login" exact-active-class="active">Войти</NuxtLink>
+		</div>
+		<nav class="navbar">
+			<ul>
+				<li>
+					<NuxtLink to="/" exact-active-class="active">Главная</NuxtLink>
+				</li>
+				<!-- <li><NuxtLink to="/about" exact-active-class="active">О нас</NuxtLink></li> -->
+				<li>
+					<NuxtLink to="/cube" exact-active-class="active">Куб</NuxtLink>
+				</li>
+				<li>
+					<NuxtLink to="/cars" exact-active-class="active">Машинки</NuxtLink>
+				</li>
+			</ul>
+		</nav>
+	</div>
 </template>
 
 <style>
-	.navbar {
+	.minimenu {
 		top: 0;
 		left: 0;
+		display: grid;
 		width: 100%;
 		background-color: #333;
 		padding: 1rem;
 	}
 
-	.navbar ul {
-		list-style: none;
+	.minimenu .navbar {
 		display: flex;
+		width: 100%;
+		justify-self: center;
+	}
+
+	.minimenu ul {
+		width: 100%;
+		list-style: none;
 		gap: 1rem;
 		margin: 0;
 		padding: 0;
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		justify-content: center;
+		align-items: center;
 	}
 
-	.navbar li {
+	.minimenu li {
 		display: inline;
+		text-align: center;
 	}
 
-	.navbar a {
+	.minimenu a {
 		text-decoration: none;
 		color: white;
 		transition: color 0.3s;
 	}
 
-	.navbar a:hover {
+	.minimenu a:hover {
 		color: #00ff00;
 	}
 
-	.navbar a.active {
+	.minimenu a.active {
 		color: #00ff00;
 		font-weight: bold;
+	}
+
+	.account {
+		text-align: right;
 	}
 </style>
