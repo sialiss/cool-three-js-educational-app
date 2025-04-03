677c87141af10eadc232cae968b010422d347429
Camellia <56206613+sialiss@users.noreply.github.com>
2025-04-03 03:32:52 +0300

    add lessons text

--- pages/Lesson/[id].vue
+++ pages/Lesson/[id].vue
@@ -2,7 +2,7 @@
 	<div class="lesson-page">
 		<template v-if="lesson">
 			<h1>{{ lesson.title }}</h1>
-			<p>{{ lesson.description }}</p>
+			<div class="lesson-text" v-html="formattedLesson"></div>
 			<button @click="goBack">Назад</button>
 		</template>
 		<p v-else>Загрузка урока...</p>
@@ -12,6 +12,7 @@
 <script setup>
 	import { ref, onMounted } from "vue"
 	import { useRoute, useRouter } from "vue-router"
+	import { marked } from "marked"
 
 	const route = useRoute()
 	const router = useRouter()
@@ -23,6 +24,14 @@
 
 		// Получаем id из URL и ищем нужный урок
 		lesson.value = lessons.find(l => l.id === Number(route.params.id))
+		const formattedLesson = computed(() => marked(lesson.lesson))
+	})
+
+	const formattedLesson = computed(() => {
+		if (lesson.value) {
+			return marked(lesson.value.lesson)
+		}
+		return ""
 	})
 
 	const goBack = () => {
@@ -30,14 +39,22 @@
 	}
 </script>
 
-<style scoped>
+<style>
 	.lesson-page {
 		width: 100%;
-        height: 100%;
+		height: 100%;
 		margin: auto;
 		padding: 20px;
 	}
+	.lesson-text {
+		margin: auto;
+		display: flex;
+		flex-direction: column;
+		white-space: pre-line;
+		gap: 0rem;
+	}
 	button {
+		margin-top: 1rem;
 		padding: 10px;
 		background: #007bff;
 		color: white;
