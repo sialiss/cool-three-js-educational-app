a8c00064c28c81b6c59403eef44753b1584b5fff
Camellia <56206613+sialiss@users.noreply.github.com>
2025-07-14 23:41:21 +0300

    update

---
+++ gh-pages.js
@@ -0,0 +1,21 @@
+import { publish } from "gh-pages"
+
+publish(
+	"dist", // path to public directory
+	{
+		branch: "gh-pages",
+		repo: "https://github.com/sialiss/cool-three-js-educational-app", // Update to point to your repository
+		user: {
+			name: "Camellia", // update to use your name
+			email: "camellia.sialiss@gmail.com", // Update to use your email
+		},
+		dotfiles: true,
+	},
+	err => {
+		if (err) {
+			console.error("Deployment error:", err)
+		} else {
+			console.log("✅ Deploy Complete!")
+		}
+	}
+)
